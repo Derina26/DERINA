089ef3a6d125687ed173ab6602119982a135624f
Derina26 <84314254+Derina26@users.noreply.github.com>
2021-05-18 23:33:05 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,3 @@
+function pinkbutton(){document.body.style.background="pink"}
+function yellowbutton(){document.body.style.background="yellow"}
+function royalbluebutton(){document.body.style.background="royalblue"}
